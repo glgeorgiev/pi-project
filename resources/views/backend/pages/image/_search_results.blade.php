8c99b1718127f2f5f3e98783b52a5615594eef8f
Georgi Georgiev <georgi.georgiev@delta.bg>
2015-12-30 15:47:22 +0200

    search results visualisation

--- resources/views/backend/pages/image/_search_results.blade.php
+++ resources/views/backend/pages/image/_search_results.blade.php
@@ -1,3 +1,6 @@
-<div>sasa</div>
-<h1>dsad</h1>
-<p>sadasdas</p>
+@foreach($images as $image)
+    <div class="col-sm-6 thumbnail" data-image_id="{{ $image->id }}">
+        {!! HTML::image($image->url, $image->title) !!}<br />
+        <p><span class="label label-default">{{ $image->title }}</span></p>
+    </div>
+@endforeach
