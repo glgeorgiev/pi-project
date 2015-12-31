163e3c6ded9f9ae9efcc8acb6613bf9503f21998
Georgi Georgiev <georgi.georgiev@delta.bg>
2015-12-31 13:49:44 +0200

    testing theme

---
+++ resources/views/frontend/partials/sidebar.blade.php
@@ -0,0 +1,24 @@
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        <h3 class="panel-title">Panel primary</h3>
+    </div>
+    <div class="panel-body">
+        Panel content
+    </div>
+</div>
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        <h3 class="panel-title">Panel primary</h3>
+    </div>
+    <div class="panel-body">
+        Panel content
+    </div>
+</div>
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        <h3 class="panel-title">Panel primary</h3>
+    </div>
+    <div class="panel-body">
+        Panel content
+    </div>
+</div>
