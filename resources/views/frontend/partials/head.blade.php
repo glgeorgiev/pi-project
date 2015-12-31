774dc988d034a3d727e1d7b99cfe88e753f0e9b4
Georgi Georgiev <georgi.georgiev@delta.bg>
2015-12-31 13:44:54 +0200

    frontend theme

--- resources/views/frontend/partials/head.blade.php
+++ resources/views/frontend/partials/head.blade.php
@@ -1,3 +1,6 @@
 <title>{{ isset($title) ? $title : 'Georgi Georgiev\'s blog' }}</title>
 
 <meta charset="utf-8">
+<meta name="description" content="{{ isset($meta_description) ? $meta_description : '' }}">
+
+{!! HTML::style('assets/css/bootstrap.journal.theme.min.css') !!}
