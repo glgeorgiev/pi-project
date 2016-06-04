de869d5ba7ebeff8f4a580b63360b19df9be0572
Georgi Georgiev <georgi.georgiev@delta.bg>
2016-06-04 17:49:50 +0300

    new migrations fix1

--- database/migrations/2016_06_04_172031_new_migrations.php
+++ database/migrations/2016_06_04_172031_new_migrations.php
@@ -11,11 +11,11 @@ class NewMigrations extends Migration
      */
     public function up()
     {
-        DB::statement('CREATE VIEW view_articles AS SELECT id, title, slug, description, content FROM articles');
+        DB::unprepared('CREATE VIEW view_articles AS SELECT id, title, slug, description, content FROM articles');
 
-        DB::statement('CREATE TRIGGER trigger_articles BEFORE UPDATE ON articles FOR EACH ROW SET NEW.updated_at = NOW()');
+        DB::unprepared('CREATE TRIGGER trigger_articles BEFORE UPDATE ON articles FOR EACH ROW SET NEW.updated_at = NOW()');
 
-        DB::statement('CREATE PROCEDURE procedure_article_views_for_user (IN param_user_id INT, OUT param_views_count INT)
+        DB::unprepared('CREATE PROCEDURE procedure_article_views_for_user (IN param_user_id INT, OUT param_views_count INT)
                         BEGIN SELECT SUM(views) INTO param_views_count FROM articles WHERE user_id = param_user_id; END');
     }
 
@@ -26,10 +26,10 @@ class NewMigrations extends Migration
      */
     public function down()
     {
-        DB::statement('DROP VIEW view_articles');
+        DB::unprepared('DROP VIEW view_articles');
 
-        DB::statement('DROP TRIGGER trigger_articles');
+        DB::unprepared('DROP TRIGGER trigger_articles');
 
-        DB::statement('DROP PROCEDURE procedure_article_views_for_user');
+        DB::unprepared('DROP PROCEDURE procedure_article_views_for_user');
     }
 }
