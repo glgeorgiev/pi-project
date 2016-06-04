ed09f93e5b18d982870d63c3c90d405ec4ed0946
Georgi Georgiev <georgi.georgiev@delta.bg>
2016-06-04 18:12:03 +0300

    new migrations fix2

--- database/migrations/2016_06_04_172031_new_migrations.php
+++ database/migrations/2016_06_04_172031_new_migrations.php
@@ -17,6 +17,21 @@ class NewMigrations extends Migration
 
         DB::unprepared('CREATE PROCEDURE procedure_article_views_for_user (IN param_user_id INT, OUT param_views_count INT)
                         BEGIN SELECT SUM(views) INTO param_views_count FROM articles WHERE user_id = param_user_id; END');
+
+        DB::unprepared('
+            CREATE EVENT event_statistics
+            ON SCHEDULE EVERY 1 WEEK STARTS \'2016-06-05 04:00:00\'
+            DO BEGIN
+                SELECT COUNT(id) INTO @users_count FROM users WHERE is_admin = 1;
+                SET @i = 0;
+                WHILE @i < @users_count DO
+                    SELECT id INTO @id FROM users WHERE is_admin = 1 LIMIT @i,1;
+                    CALL procedure_article_views_for_user(@id,@views_count);
+                    SELECT @views_count INTO OUTFILE CONCAT(\'/tmp/statistics_for_user_\', @id, \'.txt\');
+                    SET @i = @i + 1;
+                END WHILE
+            END
+        ');
     }
 
     /**
@@ -31,5 +46,7 @@ class NewMigrations extends Migration
         DB::unprepared('DROP TRIGGER trigger_articles');
 
         DB::unprepared('DROP PROCEDURE procedure_article_views_for_user');
+
+        DB::unprepared('DROP EVENT event_statistics');
     }
 }
